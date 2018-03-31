52b0848b29bf908daf041ef30cbbbc4c705f7340
Alexandre-T <alexandre.tranchant@gmail.com>
2018-03-31 22:18:03 +0200

    Repositories added.

---
+++ src/Repository/PassageRepository.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * This file is part of the LAPI application.
+ *
+ * PHP version 7.2
+ *
+ * (c) Alexandre Tranchant <alexandre.tranchant@gmail.com>
+ *
+ * @category Entity
+ *
+ * @author    Alexandre Tranchant <alexandre.tranchant@gmail.com>
+ * @license   MIT
+ *
+ * @see https://github.com/Alexandre-T/casguard/blob/master/LICENSE
+ */
+
+namespace App\Repository;
+
+use Doctrine\ORM\EntityRepository;
+
+/**
+ * The passage repository.
+ */
+class PassageRepository extends EntityRepository
+{
+}
