04072eb0d4f97292edd28caee7d6214b4551bb03
Alexandre-T <alexandre.tranchant@gmail.com>
2018-05-30 14:32:13 +0200

    AnomalieType done

---
+++ src/Entity/AnomalieType.php
@@ -0,0 +1,181 @@
+<?php
+/**
+ * This file is part of the LAPI application.
+ *
+ * PHP version 7.2
+ *
+ * (c) Alexandre Tranchant <alexandre.tranchant@gmail.com>
+ *
+ * @category Entity
+ *
+ * @author    Alexandre Tranchant <alexandre.tranchant@gmail.com>
+ * @license   MIT
+ *
+ * @see https://github.com/Alexandre-T/casguard/blob/master/LICENSE
+ */
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use ApiPlatform\Core\Annotation\ApiResource;
+
+/**
+ * Plaques et statistiques qui rencontrent des problèmes sur le type de véhicule identifié.
+ *
+ * @ORM\Entity(repositoryClass="App\Repository\PassageRepository")
+ * @ORM\Table(
+ *     name="ve_anomalie_type",
+ *     schema="data",
+ * )
+ *
+ * @ApiResource(
+ *     collectionOperations={"get"},
+ *     itemOperations={},
+ *     attributes={"order"={"identification":"DESC","ecart":"DESC","passage":"DESC"}}
+ * )
+ */
+class AnomalieType
+{
+    /**
+     * Plaque d'immatriculation.
+     *
+     * @ORM\Id
+     * @ORM\Column(type="string")
+     *
+     * @var string
+     */
+    private $immatriculation;
+
+    /**
+     * Pays d'immatriculation.
+     *
+     * @ORM\Column(type="string")
+     *
+     * @var string
+     */
+    private $pays;
+
+    /**
+     * Nombre de types distincts. (1, 2 ou 3)
+     *
+     * @ORM\Column(type="integer", nullable=false)
+     *
+     * @var int
+     */
+    private $identification;
+
+    /**
+     * Nombre de passages totaux repérés.
+     *
+     * @ORM\Column(type="integer", nullable=false)
+     *
+     * @var int
+     */
+    private $passage;
+
+    /**
+     * Nombre de passages où le véhicule a été repéré comme un camion.
+     *
+     * @ORM\Column(type="integer", nullable=false)
+     *
+     * @var int
+     */
+    private $nbCamion;
+
+    /**
+     * Nombre de passages où le véhicule a été repéré comme un véhicule léger.
+     *
+     * @ORM\Column(type="integer", nullable=false)
+     *
+     * @var int
+     */
+    private $nbVoiture;
+
+    /**
+     * Nombre de passages où le véhicule n'a pas pu être identifié.
+     *
+     * @ORM\Column(type="integer", nullable=false)
+     *
+     * @var int
+     */
+    private $nbInconnu;
+
+    /**
+     * Ecart entre le nombre d'identification VL/PL.
+     *
+     * @ORM\Column(type="integer", nullable=false)
+     *
+     * @var int
+     */
+    private $ecart;
+
+    /**
+     * Immatriculation cryptée lue sur le camion.
+     *
+     * @return string
+     */
+    public function getImmatriculation(): string
+    {
+        return $this->immatriculation;
+    }
+
+    /**
+     * Pays lu sur la plaque.
+     *
+     * @return string
+     */
+    public function getPays(): string
+    {
+        return $this->pays;
+    }
+
+    /**
+     * Récupère le nombre de passages.
+     *
+     * @return int
+     */
+    public function getPassage(): int
+    {
+        return $this->passage;
+    }
+
+    /**
+     * @return int
+     */
+    public function getIdentification(): int
+    {
+        return $this->identification;
+    }
+
+    /**
+     * @return int
+     */
+    public function getNbCamion(): int
+    {
+        return $this->nbCamion;
+    }
+
+    /**
+     * @return int
+     */
+    public function getNbVoiture(): int
+    {
+        return $this->nbVoiture;
+    }
+
+    /**
+     * @return int
+     */
+    public function getNbInconnu(): int
+    {
+        return $this->nbInconnu;
+    }
+
+    /**
+     * @return int
+     */
+    public function getEcart(): int
+    {
+        return $this->ecart;
+    }
+}
---
+++ src/Migrations/Version20180530121905.php
@@ -0,0 +1,42 @@
+<?php declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20180530121905 extends AbstractMigration
+{
+    /**
+     * Ajout de la vue sur les anomalies.
+     *
+     * @param Schema $schema
+     * @throws \Doctrine\DBAL\DBALException
+     * @throws \Doctrine\DBAL\Migrations\AbortMigrationException
+     */
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('CREATE VIEW data.ve_anomalie_type as select p.immatriculation, p.state as pays, count(distinct p.l) as identification, count(distinct p.id) as passage, sum(case p.l when 1 then 1 else 0 end) as nb_camion, sum(case p.l when -1 then 1 else 0 end) as nb_voiture, sum(case p.l when 0 then 1 else 0 end) as nb_inconnu, abs(sum(case p.l when 1 then 1 when - 1 then -1 else 0 end)) as ecart from data.te_passage p where p.data_fictive is false group by p.immatriculation, p.state having count(distinct p.l) > 1');
+    }
+
+    /**
+     * Suppression de la vue sur les anomalies.
+     *
+     * @param Schema $schema
+     * @throws \Doctrine\DBAL\DBALException
+     * @throws \Doctrine\DBAL\Migrations\AbortMigrationException
+     */
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('DROP VIEW data.ve_anomalie_type');
+    }
+}
